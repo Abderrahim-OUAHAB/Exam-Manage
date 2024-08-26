a3e5a7a9cc62fd1cafd8cc73912aa8c522f2fef7
Abderrahim-OUAHAB <abderrahimouahab5@gmail.com>
2024-08-26 11:29:23 +0100

    nice

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Remplacez 'db.json' par le nom de votre fichier JSON contenant les données.
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+server.use(cors()); // Enable CORS
 server.use(middlewares);
-server.use('/api', router); // Vous pouvez personnaliser le chemin ici
+server.use('/api', router);
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log('JSON Server is running on port', port);
+  console.log(`JSON Server is running on port ${port}`);
 });
